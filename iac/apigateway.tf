cdb70af69fc40afbc99bdd7dbd6fe7e9658d0382
Dcardenas206 <danielcardenas206@hotmail.com>
2026-03-31 14:44:32 -0500

    AWS Lambda v3

--- iac/apigateway.tf
+++ iac/apigateway.tf
@@ -5,15 +5,8 @@ resource "aws_apigatewayv2_api" "monitoring_api" {
   target        = aws_lambda_function.monitoring_api.arn
 }
 
-# 2. Configurar el "Stage" (Entorno)
-# $default hace que la API sea accesible inmediatamente en la URL base
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.monitoring_api.id
-  name        = "$default"
-  auto_deploy = true
-}
 
-# 3. Permiso para que API Gateway llame a la Lambda
+# 2. Permiso para que API Gateway llame a la Lambda
 # Sin esto, recibirás un error 500 "Internal Server Error"
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
